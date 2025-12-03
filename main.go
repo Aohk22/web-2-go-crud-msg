7ef88b558c7a61fa32a53207085afa2119b45eb6
Aohk22 <96096597+Aohk22@users.noreply.github.com>
2025-12-03 22:13:45 +0700

    db refactor, added a struct

--- main.go
+++ main.go
@@ -9,24 +9,16 @@ import (
 	"github.com/jackc/pgx/v5/pgxpool"
 )
 
-const STDIN = 0
-
-var pool *pgxpool.Pool
-
 func main() {
-	var err error
-	var dbUrl string
-	var pool *pgxpool.Pool
-
-	err = godotenv.Load()
+	err := godotenv.Load()
 	if err != nil { log.Fatal(err) }
 
-	dbUrl = os.Getenv("DATABASE_URL")
-	if len(dbUrl) == 0 { log.Fatal("env DATABASE_URL not found.") }
+	dbUrl, exist := os.LookupEnv("DATABASE_URL")
+	if !exist { log.Fatal("env DATABASE_URL not set")}
 
-	pool, err = pgxpool.New(context.Background(), dbUrl)
+	dbPool, err := pgxpool.New(context.Background(), dbUrl)
 	if err != nil { log.Fatal(err) }
-	defer pool.Close()
 
+	defer dbPool.Close()
 }
 
