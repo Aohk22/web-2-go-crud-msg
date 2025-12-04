5f2a86ce241e42c51b01525cbbb4033859c99592
Aohk22 <96096597+Aohk22@users.noreply.github.com>
2025-12-04 22:18:52 +0700

    basic server routing code & /users route impl

--- internal/srv/srv.go
+++ internal/srv/srv.go
@@ -1,24 +1,70 @@
 package srv
 
+
 import (
 	"log"
+	"fmt"
 	"net/http"
+
+	"github.com/Aohk22/web-2-go-crud-msg/internal/model"
 )
 
-func addRoutes(mux *http.ServeMux, logger *log.Logger) {
-	mux.Handle("POST /register", handleRegisterPost(logger))
-	mux.Handle("POST /login", handleRegisterPost(logger))
-	mux.Handle("GET /register", handleRegisterGet(logger))
-	mux.Handle("GET /login", handleLoginGet(logger))
-	mux.Handle("GET /user/{id}", handleUserGet(logger))
-}
 
-func NewServer(logger *log.Logger) http.Handler {
-	var mux *http.ServeMux = http.NewServeMux()
+func NewServer(
+	logger *log.Logger,
+	userStore model.UserStore,
+) http.Handler {
 	var handler http.Handler
+	var mux *http.ServeMux = http.NewServeMux()
 
-	addRoutes(mux, logger)
+	addRoutes(mux, logger, userStore)
 
 	handler = mux
 	return handler
 }
+
+
+func addRoutes(
+	mux *http.ServeMux,
+	logger *log.Logger,
+	userStore model.UserStore,
+) {
+	mux.Handle("/register", handleRegister(logger, userStore))
+	mux.Handle("/login", handleLogin(logger, userStore))
+	mux.Handle("/user/{id}", handleUserGet(logger, userStore))
+	mux.Handle("/users", handleUserGetAll(logger, userStore))
+}
+
+func handleUserGetAll(logger *log.Logger, userStore model.UserStore) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		users, err := userStore.GetAllUsers()
+		if err != nil { logger.Println(err) }
+
+		for _, user := range users {
+			logger.Println(user)
+			fmt.Fprintf(w, "%v\n", user)
+		}
+	}
+}
+
+
+func handleRegister(logger *log.Logger, userStore model.UserStore) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "unimplemented\n")
+	}
+}
+
+
+func handleUserGet(logger *log.Logger, userStore model.UserStore) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "unimplemented\n")
+	}
+}
+
+
+func handleLogin(logger *log.Logger, userStore model.UserStore) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "unimplemented\n")
+	}
+}
+
