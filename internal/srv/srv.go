5bb344aa1439c710af93b5a199ed39492269c902
Aohk22 <96096597+Aohk22@users.noreply.github.com>
2025-12-05 15:38:45 +0700

    add stores

--- internal/srv/srv.go
+++ internal/srv/srv.go
@@ -2,9 +2,12 @@ package srv
 
 
 import (
-	"log"
+	"encoding/json"
 	"fmt"
+	"io"
+	"log"
 	"net/http"
+	"strconv"
 
 	"github.com/Aohk22/web-2-go-crud-msg/internal/model"
 )
@@ -13,13 +16,17 @@ import (
 func NewServer(
 	logger *log.Logger,
 	userStore model.UserStore,
+	messageStore model.MessageStore,
+	roomStore model.RoomStore,
 ) http.Handler {
 	var handler http.Handler
 	var mux *http.ServeMux = http.NewServeMux()
 
-	addRoutes(mux, logger, userStore)
+	addRoutes(mux, logger, userStore, messageStore, roomStore)
 
+	// TODO: wrap the loggers and auth middleware
 	handler = mux
+
 	return handler
 }
 
@@ -28,43 +35,88 @@ func addRoutes(
 	mux *http.ServeMux,
 	logger *log.Logger,
 	userStore model.UserStore,
+	messageStore model.MessageStore,
+	roomStore model.RoomStore,
 ) {
-	mux.Handle("/register", handleRegister(logger, userStore))
-	mux.Handle("/login", handleLogin(logger, userStore))
-	mux.Handle("/user/{id}", handleUserGet(logger, userStore))
-	mux.Handle("/users", handleUserGetAll(logger, userStore))
+	mux.Handle("/register", handleRegister(userStore))
+	mux.Handle("/login", handleLogin(userStore))
+	mux.Handle("/user/{id}", handleUserGet(userStore))
+	mux.Handle("/users", handleUserGetAll(userStore))
+
+	mux.Handle("/message/{id}", handleMessageGet(messageStore))
+
+	mux.Handle("/room/{id}", handleRoomGet(roomStore))
 }
 
-func handleUserGetAll(logger *log.Logger, userStore model.UserStore) http.HandlerFunc {
+
+// --- user handlers ---
+
+
+func handleUserGet(userStore model.UserStore) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		idStr := r.PathValue("id")
+		id, err := strconv.ParseInt(idStr, 10, 16)
+		if err != nil { http.Error(w, "cant convert id in param", 500) }
+		
+		user, err := userStore.GetUser(uint16(id))
+		if err != nil { http.Error(w, "cant get user", 500) }
+		fmt.Fprintf(w, "%v\n", user)
+	}
+}
+
+
+func handleUserGetAll(userStore model.UserStore) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		users, err := userStore.GetAllUsers()
-		if err != nil { logger.Println(err) }
+		if err != nil { http.Error(w, err.Error(), 500) }
 
 		for _, user := range users {
-			logger.Println(user)
 			fmt.Fprintf(w, "%v\n", user)
 		}
 	}
 }
 
 
-func handleRegister(logger *log.Logger, userStore model.UserStore) http.HandlerFunc {
+func handleRegister(userStore model.UserStore) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		if r.Method == "POST" {
+			data, err := io.ReadAll(r.Body)
+			if err != nil { http.Error(w, "error reading json", 500) }
+			if validJson := json.Valid(data); !validJson { http.Error(w, "not valid json", 500) }
+			var user model.User
+			if err := json.Unmarshal(data, &user); err != nil {
+				http.Error(w, "error parsing json", 500)
+			}
+			stat, err := userStore.AddUser(user.Name, user.Password)
+			if err != nil { http.Error(w, "add user error", 500) }
+			fmt.Fprintf(w, "%s\n", stat)
+		} else {
+			http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+		}
+	}
+}
+
+
+func handleLogin(userStore model.UserStore) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "unimplemented\n")
 	}
 }
 
 
-func handleUserGet(logger *log.Logger, userStore model.UserStore) http.HandlerFunc {
+// --- messages handlers ---
+
+
+func handleMessageGet(messageStore model.MessageStore) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "unimplemented\n")
+		idStr := r.PathValue("id")
+		id, err := strconv.ParseInt(idStr, 10, 16)
+		if err != nil { http.Error(w, "cant convert id in param", 500) }
+
+		var getMsgStrat = &model.GetMessageByUser{ UserId: uint16(id) }
+		msg, err := messageStore.GetMessage(getMsgStrat)
+		if err != nil { http.Error(w, "cant get message", 500) }
+
+		fmt.Fprintf(w, "%v\n", msg)
 	}
 }
-
-
-func handleLogin(logger *log.Logger, userStore model.UserStore) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "unimplemented\n")
-	}
-}
-
