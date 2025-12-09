1856fad1db26b00bcea294dd87006a6137d095eb
Aohk22 <96096597+Aohk22@users.noreply.github.com>
2025-12-09 15:59:59 +0700

    all routes writes marshal'd str to response

--- internal/srv/routing.go
+++ internal/srv/routing.go
@@ -1,6 +1,7 @@
 package srv
 
 import (
+	"context"
 	"encoding/json"
 	"fmt"
 	"io"
@@ -10,142 +11,162 @@ import (
 	"github.com/Aohk22/web-2-go-crud-msg/internal/model"
 )
 
-func addRoutes(mux *http.ServeMux, stores *Stores) {
-	mux.Handle("/register", handleRegister(stores.UserStore))
-	mux.Handle("/login", handleLogin(stores.UserStore))
 
-	mux.Handle("/users", handleUserGetAll(stores.UserStore))
-	mux.Handle("/rooms", handleRoomGetAll(stores.RoomStore))
+func addRoutes(ctx context.Context, mux *http.ServeMux, stores *Stores) {
+	mux.Handle("/register", handleRegister(ctx, stores.UserStore))
+	mux.Handle("/login", handleLogin(ctx, stores.UserStore))
 
-	mux.Handle("/user/{id}", handleUserGet(stores.UserStore))
-	mux.Handle("/user/{id}/messages", handleMessageGet(stores.MessageStore))
+	mux.Handle("/users", handleUserGetAll(ctx, stores.UserStore))
+	mux.Handle("/rooms", handleRoomGetAll(ctx, stores.RoomStore))
 
-	mux.Handle("/room/{id}", handleRoomGet(stores.RoomStore))
+	mux.Handle("/user/{id}", handleUserGet(ctx, stores.UserStore))
+	mux.Handle("/user/{id}/messages", handleMessageGet(ctx, stores.MessageStore))
 
-	mux.Handle("/room/{id}/users", handleRoomGetUsers(stores.UserStore))
-	mux.Handle("/room/{id}/messages", handleRoomGetMessages(stores.MessageStore))
+	mux.Handle("/room/{id}", handleRoomGet(ctx, stores.RoomStore))
+
+	mux.Handle("/room/{id}/users", handleRoomGetUsers(ctx, stores.UserStore))
+	mux.Handle("/room/{id}/messages", handleRoomGetMessages(ctx, stores.MessageStore))
 }
 
 
-func handleUserGet(userStore model.UserStore) http.HandlerFunc {
+func handleUserGet(ctx context.Context, userStore model.UserStore) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		idStr := r.PathValue("id")
 		id, err := strconv.ParseInt(idStr, 10, 16)
 		if err != nil { http.Error(w, "cant convert id in param", 500) }
 		
-		user, err := userStore.GetUser(uint32(id))
+		user, err := userStore.GetUser(ctx, uint32(id))
 		if err != nil { http.Error(w, "cant get user", 500) }
-		fmt.Fprintf(w, "%v\n", user)
+
+		json, err := json.Marshal(user)
+		if err != nil { http.Error(w, "cant convert json", 500) }
+
+		fmt.Fprintf(w, "%s", json)
 	}
 }
 
 
-func handleUserGetAll(userStore model.UserStore) http.HandlerFunc {
+func handleUserGetAll(ctx context.Context, userStore model.UserStore) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		users, err := userStore.GetAllUsers()
+		users, err := userStore.GetAllUsers(ctx)
 		if err != nil { http.Error(w, err.Error(), 500) }
 
-		for _, user := range users {
-			fmt.Fprintf(w, "%d - %s - %s\n", user.Id, user.Name, user.Password)
-		}
+		json, err := json.Marshal(users)
+		if err != nil { http.Error(w, "cant convert json", 500) }
+
+		fmt.Fprintf(w, "%s", json)
 	}
 }
 
 
-func handleRegister(userStore model.UserStore) http.HandlerFunc {
+func handleRegister(ctx context.Context, userStore model.UserStore) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		if r.Method == "POST" {
+			var user model.User
+
 			data, err := io.ReadAll(r.Body)
 			if err != nil { http.Error(w, "error reading json", 500) }
 			if validJson := json.Valid(data); !validJson { http.Error(w, "not valid json", 500) }
-			var user model.User
+
 			if err := json.Unmarshal(data, &user); err != nil {
 				http.Error(w, "error parsing json", 500)
 			}
-			stat, err := userStore.AddUser(user.Name, user.Password)
+
+			stat, err := userStore.AddUser(ctx, user.Name, user.Password)
 			if err != nil { http.Error(w, "add user error", 500) }
-			fmt.Fprintf(w, "%s\n", stat)
+			fmt.Fprintf(w, "%s", stat)
 		} else {
 			http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
 		}
 	}
 }
 
-func handleLogin(userStore model.UserStore) http.HandlerFunc {
+
+func handleLogin(ctx context.Context, userStore model.UserStore) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "unimplemented\n")
+		fmt.Fprintf(w, "unimplemented")
+		panic("unimplemented")
 	}
 }
 
 
-func handleMessageGet(messageStore model.MessageStore) http.HandlerFunc {
+func handleMessageGet(ctx context.Context, messageStore model.MessageStore) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		idStr := r.PathValue("id")
 		id, err := strconv.ParseInt(idStr, 10, 16)
 		if err != nil { http.Error(w, "cant convert id in param", 500) }
 
 		var getMsgStrat = &model.GetMessageByUser{ UserId: uint32(id) }
-		msg, err := messageStore.GetMessage(getMsgStrat)
+		msg, err := messageStore.GetMessage(ctx, getMsgStrat)
 		if err != nil { http.Error(w, "cant get message", 500) }
 
-		fmt.Fprintf(w, "%v\n", msg)
+		json, err := json.Marshal(msg)
+		if err != nil { http.Error(w, "cant convert json", 500) }
+
+		fmt.Fprintf(w, "%s", json)
 	}
 }
 
 
-func handleRoomGet(roomStore model.RoomStore) http.HandlerFunc {
+func handleRoomGet(ctx context.Context, roomStore model.RoomStore) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		idStr := r.PathValue("id")
 		id, err := strconv.ParseInt(idStr, 10, 16)
 		if err != nil { http.Error(w, "cant convert id in param", 500) }
 
-		room, err := roomStore.GetRoom(uint32(id))
+		room, err := roomStore.GetRoom(ctx, uint32(id))
 		if err != nil { http.Error(w, "can get room", 500) }
 
-		fmt.Fprintf(w, "%v\n", room)
+		json, err := json.Marshal(room)
+		if err != nil { http.Error(w, "cant convert json", 500) }
+
+		fmt.Fprintf(w, "%s", json)
 	}
 }
 
 
-func handleRoomGetAll(roomStore model.RoomStore) http.HandlerFunc {
+func handleRoomGetAll(ctx context.Context, roomStore model.RoomStore) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		rooms, err := roomStore.GetAllRooms()
+		rooms, err := roomStore.GetAllRooms(ctx)
 		if err != nil { http.Error(w, "cannot get all rooms", 500) }
 
-		for _, room := range rooms {
-			fmt.Fprintf(w, "Room name: %s\nCreation date: %v\n\n", room.Name, room.Time)
-		}
+		json, err := json.Marshal(rooms)
+		if err != nil { http.Error(w, "cant convert json", 500) }
+
+		fmt.Fprintf(w, "%s", json)
 	}
 }
 
 
-func handleRoomGetUsers(userStore model.UserStore) http.HandlerFunc {
+func handleRoomGetUsers(ctx context.Context, userStore model.UserStore) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		roomIdStr := r.PathValue("id")
 		roomIdInt, err := strconv.ParseInt(roomIdStr, 10, 16)
 		if err != nil { http.Error(w, "could not parse int.", 500) }
 
-		users, err := userStore.GetUsersByRoom(uint32(roomIdInt))
+		users, err := userStore.GetUsersByRoom(ctx, uint32(roomIdInt))
 		if err != nil { http.Error(w, "could not get users by room", 500) }
 
-		for _, user := range users {
-			fmt.Fprintf(w, "Room %d: %v\n", roomIdInt, user)
-		}
+		json, err := json.Marshal(users)
+		if err != nil { http.Error(w, "cant convert json", 500) }
+
+		fmt.Fprintf(w, "%s", json)
 	}
 }
 
 
-func handleRoomGetMessages(messageStore model.MessageStore) http.HandlerFunc {
+func handleRoomGetMessages(ctx context.Context, messageStore model.MessageStore) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		roomIdStr := r.PathValue("id")
 		roomIdInt, err := strconv.ParseInt(roomIdStr, 10, 16)
 
 		strat := &model.GetMessageByRoom { RoomId: uint32(roomIdInt)}
-		messages, err := messageStore.GetMessage(strat)
+		messages, err := messageStore.GetMessage(ctx, strat)
 		if err != nil { http.Error(w, "cannot get message by room", 500) }
 
-		for _, message := range messages {
-			fmt.Fprintf(w, "message: %v\n", message)
-		}
+		json, err := json.Marshal(messages)
+		if err != nil { http.Error(w, "cant convert json", 500) }
+
+		fmt.Fprintf(w, "%s", json)
 	}
 }
