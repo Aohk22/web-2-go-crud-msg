857730b03c694f88fa6dfbd0458c1b8e69667552
Aohk22 <96096597+Aohk22@users.noreply.github.com>
2025-12-23 18:30:14 +0700

    wrap all content in parent div

--- web-ui/src/App.tsx
+++ web-ui/src/App.tsx
@@ -1,8 +1,8 @@
 import { BrowserRouter, Navigate, Route, Routes } from 'react-router';
 import { Login, Logout, Home, Room } from './routes';
 import { checkLogin } from './lib/utils.ts';
-import './routes/Styles.css'
 import type { JSX } from 'react';
+import './Styles.css'
 
 function RequireAuth({ children } : { children: JSX.Element }) {
 	return checkLogin() ? children : <Navigate to='/login' replace />;
@@ -10,19 +10,21 @@ function RequireAuth({ children } : { children: JSX.Element }) {
 
 export default function App() {
 	return (
-		<BrowserRouter>
-		<Routes>
-			<Route path='/login' element={<Login />} /> 
-			<Route path='/logout' element={<Logout />} />
-			<Route path='/' element={
-				<RequireAuth children={<Home />} />
-			} />
-			<Route path='/room/:id' element={
-				<RequireAuth children={<Room />} />
-			} />
-			<Route path='*' element={<Navigate to='/' replace />} />
-		</Routes>
-		</BrowserRouter>
+		<div className='contentBlock'>
+			<BrowserRouter>
+			<Routes>
+				<Route path='/login' element={<Login />} /> 
+				<Route path='/logout' element={<Logout />} />
+				<Route path='/' element={
+					<RequireAuth children={<Home />} />
+				} />
+				<Route path='/room/:id' element={
+					<RequireAuth children={<Room />} />
+				} />
+				<Route path='*' element={<Navigate to='/' replace />} />
+			</Routes>
+			</BrowserRouter>
+		</div>
 	);
 }
 
